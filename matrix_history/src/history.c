554aec4d520fcd6bcb8256e89b377423d1e595a6
Felimikli <miklikowskifr@gmail.com>
2024-09-10 18:39:13 -0300

    changed systems names

--- matrix_history/src/history.c
+++
@@ -1,5 +0,0 @@
-#include "history.h"
-#include "utils.h"
-
-void matrix_history() {
-}
--- ui/include/temp.h
+++
@@ -1,4 +0,0 @@
-#include <stdbool.h>
-
-void show_ui();
-void print_options(bool is_square, bool is_non_singular);
